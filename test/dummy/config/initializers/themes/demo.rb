44cec75413f86587174aa30d150c181ee4aadb11
Matt Redmond <signs.of.antilife@gmail.com>
2018-03-30 17:35:29 +1030

    Ran install generator inside  to create themes/etc.

--- test/dummy/config/initializers/themes/demo.rb
+++ test/dummy/config/initializers/themes/demo.rb
@@ -1,24 +1,71 @@
 ::Spina::Theme.register do |theme|
 
   theme.name = 'demo'
-  theme.title = 'Demo Theme'
+  theme.title = 'Demo theme'
 
   theme.page_parts = [{
+    name:           'line',
+    title:          'Line',
+    partable_type:  'Spina::Line'
+  }, {
     name:           'text',
     title:          'Text',
     partable_type:  'Spina::Text'
+  }, {
+    name:           'photo',
+    title:          'Photo',
+    partable_type:  'Spina::Photo'
+  }, {
+    name:           'photo_collection',
+    title:          'Photo collection',
+    partable_type:  'Spina::PhotoCollection'
+  }, {
+    name:           'attachment',
+    title:          'Attachment',
+    partable_type:  'Spina::Attachment'
+  }, {
+    name:           'attachment_collection',
+    title:          'Attachment collection',
+    partable_type:  'Spina::AttachmentCollection'
+  }, {
+    name:           'structure',
+    title:          'Structure',
+    partable_type:  'Spina::Structure'
+  }]
+
+  theme.structures = [{
+    name: 'structure',
+    structure_parts: [{
+      name:           'title',
+      title:          'Title',
+      partable_type:  'Spina::Line'
+    }, {
+      name:           'description',
+      title:          'Description',
+      partable_type:  'Spina::Text'
+    }]
+  }]
+
+  theme.layout_parts = [{
+    name:           'line',
+    title:          'Line',
+    partable_type:  'Spina::Line'
   }]
 
   theme.view_templates = [{
-    name:       'homepage',
-    title:      'Homepage',
-    page_parts: ['text']
+    name: 'homepage',
+    title: 'Homepage',
+    page_parts: ['text'],
   }, {
     name: 'show',
-    title:        'Default',
-    description:  'A simple page',
-    usage:        'Use for your content',
-    page_parts:   ['text']
+    title: 'Default',
+    usage: 'Use for your content',
+    page_parts: ['text']
+  }, {
+    name: 'demo',
+    title: 'Demo',
+    description: 'Contains examples of every page part',
+    page_parts: ['line', 'text', 'photo', 'photo_collection', 'attachment', 'attachment_collection', 'structure']
   }]
 
   theme.custom_pages = [{
@@ -26,15 +73,13 @@
     title:          'Homepage',
     deletable:      false,
     view_template:  'homepage'
+  }, {
+    name:           'demo',
+    title:          'Demo',
+    deletable:      true,
+    view_template:  'demo'
   }]
 
-  theme.navigations = [{
-    name: 'mobile',
-    label: 'Mobile'  
-  }, {
-    name: 'main',
-    label: 'Main navigation',
-    auto_add_pages: true
-  }]
+  theme.plugins = ['reviews']
 
 end
--- test/factories/accounts.rb
+++ test/factories/accounts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :account, class: Spina::Account do
-    name 'My Website'
-    preferences { { theme: 'awesome' } }
+    name 'MySite'
+    preferences { { theme: 'demo' } }
   end
 end
